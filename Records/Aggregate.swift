713a34c9df0af94b6edcfa9009357888c00282ef
Rob Nash <robscode@icloud.com>
2018-06-11 14:06:33 +0100

    Removing depracation

--- Records/Aggregate.swift
+++ Records/Aggregate.swift
@@ -38,39 +38,3 @@ public struct Aggregate<T: NSManagedObject> {
   }
   
 }
-
-@available(*, deprecated: 3.1.0, message: "Please use Aggregate<T: NSManagedObject>")
-public struct RelationshipRestriction {
-  /// Inclusive operators. As per https://developer.apple.com/library/content/documentation/Cocoa/Conceptual/Predicates/Articles/pSyntax.html
-  ///
-  /// - someMatching: Must include at least one.
-  /// - allMatching: Must include all.
-  /// - noneMatching: Must exclude all.
-  public enum Aggregate {
-    case someMatching
-    case allMatching
-    case noneMatching
-  }
-  
-  public let operation: Aggregate
-  public let records: Set<NSManagedObject>
-  
-  public init(operation: Aggregate, records: Set<NSManagedObject>) {
-    self.operation = operation
-    self.records = records
-  }
-  
-  public func predicate(_ name: String) -> NSPredicate {
-    switch operation {
-    case .allMatching:
-      /// https://stackoverflow.com/a/47001325/1951992
-      return NSPredicate(format: "SUBQUERY(\(name), $p, $p in %@).@count = %d", records, records.count)
-    case .noneMatching:
-      /// https://stackoverflow.com/a/19716571/1951992
-      return NSPredicate(format: "SUBQUERY(\(name), $p, $p in %@).@count == 0", records)
-    case .someMatching:
-      return NSPredicate(format: "ANY " + name + " " + "IN" + " " + "%@", records)
-    }
-  }
-  
-}
